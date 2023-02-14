c40476a4f748194f2de06abd6c12c899e3bca8c5
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-02-15 01:08:29 +0500

    cat and dog

--- dogs.py
+++ dogs.py
@@ -1,4 +1,3 @@
-from pprint import pprint
 import time
 import requests
 TOKEN ='6107593018:AAEUBCT15POSGPilNPZ4ucoFv_3AsWHgkEE'
@@ -7,10 +6,16 @@ def get_updates():
     r=requests.get(f'https://api.telegram.org/bot{TOKEN}/GetUpdates')
     data=r.json()['result'][-1]
     return data
+     
 def get_dogs():
     r=requests.get('https://random.dog/woof.json/')
     dog=r.json()['url']
     return dog
+def get_cats():
+    r=requests.get('https://aws.random.cat/meow')
+    cat=r.json()['file']
+    return cat
+     
 def send_photo(chat_id,photo):
     param={'chat_id':chat_id,
            'photo':photo
@@ -19,24 +24,33 @@ def send_photo(chat_id,photo):
     r=requests.get(f'https://api.telegram.org/bot{TOKEN}/sendPhoto',params=param)
     ans=r.json()
     return ans
-def send_message(chat_id,text):
-    param={'chat_id':chat_id,
-           'photo':text
-          }
+def send_message():
+    keyboard=[[{'text':'dog🐶'}],[{'text':'cat😺'}],[{'text':'_🐶_Tepaga bosing_😺_'}]]
+    reply_markup={'keyboard':keyboard}
+    param={
+        'chat_id':get_updates()['message']['chat']['id'],
+        'text':'/start',
+        'reply_markup':reply_markup
 
-    r=requests.get(f'https://api.telegram.org/bot{TOKEN}/sendMessage',params=param)
-    ans=r.json()
+    }
+
+    r=requests.post(f'https://api.telegram.org/bot{TOKEN}/sendMessage',json=param)
+    ans=r.json()['result']
     return ans
+print(send_message())
 last_updates_id=-1
 while True:
         dogs=get_dogs()
+        cats=get_cats()
         upp=get_updates()
         chat_id=upp['message']['chat']['id']
         updates_id=upp['update_id']
         if updates_id!=last_updates_id:
             text=upp['message']['text']
-
-            if text=='/dog':
+            print(chat_id)
+            if text=='dog🐶':
                 send_photo(chat_id,dogs)
+            if text=='cat😺':
+                 send_photo(chat_id,cats)
             last_updates_id=updates_id
         time.sleep(2)
