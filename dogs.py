c72d03e675620496a6753a89fd9c3d2de0a148a7
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-02-16 12:26:27 +0500

    Cats and Dogs

--- dogs.py
+++ dogs.py
@@ -1,6 +1,6 @@
 import time
 import requests
-TOKEN ='6107593018:AAEUBCT15POSGPilNPZ4ucoFv_3AsWHgkEE'
+TOKEN ='6138073029:AAEKI-9AA9pxG_goL9l8soAhff0GD0IqX7g'
 #GET Updates
 def get_updates():
     r=requests.get(f'https://api.telegram.org/bot{TOKEN}/GetUpdates')
@@ -24,12 +24,12 @@ def send_photo(chat_id,photo):
     r=requests.get(f'https://api.telegram.org/bot{TOKEN}/sendPhoto',params=param)
     ans=r.json()
     return ans
-def send_message():
-    keyboard=[[{'text':'dog🐶'}],[{'text':'cat😺'}],[{'text':'_🐶_Tepaga bosing_😺_'}]]
+def send_message(chat_id):
+    keyboard=[[{'text':'dog🐶'}],[{'text':'cat😺'}]]
     reply_markup={'keyboard':keyboard}
     param={
-        'chat_id':get_updates()['message']['chat']['id'],
-        'text':'/start',
+        'chat_id':chat_id,
+        'text':'Pastki panelga o`ting',
         'reply_markup':reply_markup
 
     }
@@ -37,17 +37,20 @@ def send_message():
     r=requests.post(f'https://api.telegram.org/bot{TOKEN}/sendMessage',json=param)
     ans=r.json()['result']
     return ans
-print(send_message())
 last_updates_id=-1
 while True:
         dogs=get_dogs()
         cats=get_cats()
         upp=get_updates()
+        print(upp)
         chat_id=upp['message']['chat']['id']
+        text=upp['message']['text']
         updates_id=upp['update_id']
+
+        
         if updates_id!=last_updates_id:
-            text=upp['message']['text']
-            print(chat_id)
+            if text=='/start':
+                send_message(chat_id)
             if text=='dog🐶':
                 send_photo(chat_id,dogs)
             if text=='cat😺':
